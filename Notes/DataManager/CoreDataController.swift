18f43c84cc3ef80cd4e8ff39d2f4c9c02f25e313
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-01-29 01:25:16 +0300

    Add CoreData

---
+++ Notes/DataManager/CoreDataController.swift
@@ -0,0 +1,55 @@
+//
+//  CoreDataController.swift
+//  Notes
+//
+//  Created by Pavel on 29.01.23.
+//
+
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Note")
+    
+    init() {
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+    
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+        } catch {
+            context.rollback()
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    func addNote(message: String, date: Date, color: String, context: NSManagedObjectContext) {
+        let note = Note(context: context)
+        
+        note.id = UUID()
+        note.message = message
+        note.date = date
+        note.color = color
+        
+        save(context: context)
+    }
+    
+    func editNote(note: Note, message: String, date: Date, color: String, context: NSManagedObjectContext) {
+        note.message = message
+        note.date = date
+        note.color = color
+        
+        save(context: context)
+    }
+    
+    func deleteNote(note: Note, context: NSManagedObjectContext) {
+        context.delete(note)
+        
+        save(context: context)
+    }
+}
