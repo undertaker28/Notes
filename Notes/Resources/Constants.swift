b6e0606e63870a470bfd901b516ddfc3be3df46b
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-01-29 01:44:59 +0300

    Add constants

---
+++ Notes/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Notes
+//
+//  Created by Pavel on 29.01.23.
+//
+
+import Foundation
+
+enum Constants {
+    static let colors = ["Blue", "Green", "Orange", "Purple", "Skin"]
+}
