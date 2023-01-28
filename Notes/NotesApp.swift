1c60821915d6ad18a0a3d19fd3ec0e9815607094
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-01-29 01:03:45 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Pavel on 29.01.23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
